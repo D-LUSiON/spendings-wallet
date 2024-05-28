626882e4b849b5ec6cbdcba17b0636bfc88c0558
Lubomir Peikov <l.peikov@gmail.com>
2024-05-28 11:04:11 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spendings-wallet',
+  webDir: 'www'
+};
+
+export default config;
